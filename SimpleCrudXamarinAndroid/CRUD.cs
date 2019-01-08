a8955e75e8794e4cc537e28c1e3af91ad6817817
akidatawaka <tawakida@icloud.com>
2019-01-08 13:52:37 +0700

    Commit pertama belum menggunakan database

---
+++ SimpleCrudXamarinAndroid/CRUD.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Collections;
+
+namespace SimpleCrudXamarinAndroid
+{
+    class CRUD
+    {
+        ArrayList players;
+        ArrayAdapter adapter;
+
+        public CRUD(ArrayList players, ArrayAdapter adapter)
+        {
+            this.players = players;
+            this.adapter = adapter;
+        }
+
+        public Boolean Add(String name)
+        {
+            try
+            {
+                players.Add(name);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Boolean Update(String newName, int id)
+        {
+            try
+            {
+                players.RemoveAt(id);
+                players.Insert(id, newName);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public Boolean Delete (int id)
+        {
+            try
+            {
+                players.RemoveAt(id);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public void Clear()
+        {
+            players.Clear();
+        }
+    }
+}
---
+++ SimpleCrudXamarinAndroid/MainActivity.cs
@@ -0,0 +1,101 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+using Android.Content;
+using System.Collections;
+using System;
+
+namespace SimpleCrudXamarinAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        ArrayList names;
+        ListView listView1;
+        ArrayAdapter adapter;
+        CRUD crud;
+        EditText txtName;
+        Button btnAdd, btnUpdate, btnDelete, btnClear;
+        int selectedItem = -1;
+
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            listView1 = FindViewById<ListView>(Resource.Id.listView1);
+            txtName = FindViewById<EditText>(Resource.Id.txtName);
+            btnAdd = FindViewById<Button>(Resource.Id.btnAdd);
+            btnUpdate = FindViewById<Button>(Resource.Id.btnUpdate);
+            btnDelete = FindViewById<Button>(Resource.Id.btnDelete);
+            btnClear = FindViewById<Button>(Resource.Id.btnClear);
+
+            names = new ArrayList();
+
+            adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, names);
+            listView1.Adapter = adapter;
+
+            crud = new CRUD(names, adapter);
+
+            listView1.ItemClick += ListView1_ItemClick;
+            btnAdd.Click += BtnAdd_Click;
+            btnUpdate.Click += BtnUpdate_Click;
+            btnDelete.Click += BtnDelete_Click;
+            btnClear.Click += BtnClear_Click;
+        }
+
+        private void BtnClear_Click(object sender, EventArgs e)
+        {
+            crud.Clear();
+            txtName.Text = "";
+            adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, names);
+            listView1.Adapter = adapter;
+            Toast.MakeText(this, "Button Clear Pressed", ToastLength.Short).Show();
+        }
+
+        private void BtnDelete_Click(object sender, EventArgs e)
+        {
+            if (crud.Delete(selectedItem))
+            {
+                txtName.Text = "";
+                adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, names);
+                listView1.Adapter = adapter;
+                Toast.MakeText(this, "Button Delete Pressed", ToastLength.Short).Show();
+            }
+            
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            if (crud.Add(txtName.Text))
+            {
+                txtName.Text = "";
+                adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, names);
+                listView1.Adapter = adapter;
+                Toast.MakeText(this, "Button Add Pressed", ToastLength.Short).Show();
+            }            
+        }
+
+        private void ListView1_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            this.selectedItem = e.Position;
+            txtName.Text = names[selectedItem].ToString();
+        }
+
+        private void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (crud.Update(txtName.Text, selectedItem))
+            {
+                txtName.Text = "";
+                adapter = new ArrayAdapter(this, Android.Resource.Layout.SimpleListItem1, names);
+                listView1.Adapter = adapter;
+                Toast.MakeText(this, "Button Update Pressed", ToastLength.Short).Show();
+            }
+        }
+    }
+}
